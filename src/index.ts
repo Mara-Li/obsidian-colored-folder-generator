49ccd3281a76db6f350fb457a382b7dc3b25678d
Lisandra Simonetti <l.simonetti@stragen.fr>
2024-11-22 16:20:59 +0100

    chore:  first step

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,18 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import {config} from "../env";
 
+const template = {
+	css: fs.readFileSync(path.join(__dirname, 'template', 'styles.scss'), 'utf-8'),
+	settings: fs.readFileSync(path.join(__dirname, 'template', 'settings.yml'), 'utf-8'),
+}
 
+function generate(folderName: string, number: number, template: string): string {
+	return template.replace(/\{\{FOLDER}}/g, folderName).replace(/\{\{FOLDER_NUMBER}}/g, number.toString());
+}
+
+function createScss() {
+	let output = '';
+	for (const folder of config) {
+		const folder =
+	}
+}
